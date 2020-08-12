9f39a4bd25bf98c4fb5bc7596b4d7aef9aad6496
Adyatan <techynepal@gmail.com>
2020-08-12 16:04:07 +0545

    code fix

--- stealth/modules/antiflood.py
+++ stealth/modules/antiflood.py
@@ -27,7 +27,7 @@ def check_flood(bot: Bot, update: Update) -> str:
 
     # ignore admins and whitelists
     if (is_user_admin(chat, user.id)
-            or user.id in WHITELIST_USERS:
+            or user.id in WHITELIST_USERS):
         sql.update_flood(chat.id, None)
         return log_message
 
